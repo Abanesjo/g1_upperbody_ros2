3eb17ee09e56c7fd43b20ae2d85b20d36e58c489
John Abanes <jya9538@nyu.edu>
2026-05-20 11:52:33 +0400

    move cyclonedds to make docker cache more efficient

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -17,7 +17,7 @@ if ! grep -qxF "#Entrypoint Setup" ~/.bashrc; then
 source /opt/ros/humble/setup.bash
 source /workspace/ros2_ws/install/setup.bash
 export RMW_IMPLEMENTATION="rmw_cyclonedds_cpp"
-export CYCLONEDDS_URI=file:///workspace/dependencies/cyclonedds.xml
+export CYCLONEDDS_URI=file:///workspace/ros2_ws/src/cyclonedds.xml
 alias mujoco='export LD_LIBRARY_PATH=/usr/local/lib:/usr/lib/x86_64-linux-gnu'
 export XLA_PYTHON_CLIENT_MEM_FRACTION=".50"
 EOF
