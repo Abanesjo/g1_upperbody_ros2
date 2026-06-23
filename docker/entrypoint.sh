6aee8794f488c30d4f01fc9829adf2f4dc652ebf
Vicon <john.abanes@nyu.edu>
2026-06-23 11:13:20 +0400

    update domain id

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -16,6 +16,7 @@ if ! grep -qxF "#Entrypoint Setup" ~/.bashrc; then
 #Entrypoint Setup
 source /opt/ros/humble/setup.bash
 source /workspace/ros2_ws/install/setup.bash
+export ROS_DOMAIN_ID=34
 export RMW_IMPLEMENTATION="rmw_cyclonedds_cpp"
 export CYCLONEDDS_URI=file:///workspace/ros2_ws/src/cyclonedds.xml
 alias mujoco='export LD_LIBRARY_PATH=/usr/local/lib:/usr/lib/x86_64-linux-gnu'
